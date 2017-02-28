27f8fc9902167a51d922c73ffe8caca69236a6fe
Brojandos <Brojandos@gmail.com>
2017-02-28 18:52:06 +0600

    Version 2 - Remove helping methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * @author: Brojandos.
  * @creation_date: 21.02.2017.
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        int[] salaryValues = getValuesFromInput();
-        System.out.println(getDifferenceFromMinMaxValues(salaryValues));
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt(), b = input.nextInt(), c = input.nextInt();
+        int x = Math.abs(a - b), y = Math.abs(b - c), z = Math.abs(a - c);
+        System.out.print(x > y ? (x > z ? x : z) : (y > z ? y : z));
     }
-
-    private static int getDifferenceFromMinMaxValues(int[] values) {
-        int min = values[0], max = values[0];
-        for (int i = 1; i < values.length; i++) {
-            if (min > values[i]) {
-                min = values[i];
-                continue;
-            }
-            if (max < values[i]) max = values[i];
-        }
-        return max - min;
-    }
-
-    private static int[] getValuesFromInput() throws IOException {
-        String[] stringValues = new BufferedReader(new InputStreamReader(System.in)).readLine().split(" ");
-        int[] values = new int[stringValues.length];
-        for (int i = 0; i < values.length; i++)
-            values[i] = Integer.parseInt(stringValues[i]);
-        return values;
-    }
-
-
 }
